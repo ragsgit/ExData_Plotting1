040d0978d8d1bbc43eab7ddc9583d1576c974421
Rag Ram <ragr@yahoo.com>
2017-10-04 21:52:06 -0700

    corrected output file name, and comment typos

--- plot1.R
+++ plot1.R
@@ -8,6 +8,7 @@ destFile <- "eda1data.zip"
 dataFile <- "household_power_consumption.txt"
 dateFrom <- "1/2/2007"
 dateTo   <- "2/2/2007"
+outPutFile <- "plot1.png"
 
 if (!file.exists(dataFile)) {
   message("File not found, downloading...")
@@ -20,13 +21,13 @@ if (!file.exists(dataFile)) {
 if (!file.exists(dataFile)) {
   stop("Fatal error, file not downloaded or unzip issue!")
 }
-message("File data being loaded...")
+message("File data being read into memory...")
 # read data from unzip fie household_power_consumption.txt,  NA = "?"
 # we will read the whole data and then subset later
 eda1data_full <- read.table(dataFile, header=TRUE, na.strings="?", sep=";")
 # subset the required data
 # select the 2 day data Feb 1 to Feb 2, 2007
-message("File data loaded, subsetting for data range...")
+message("File data loaded into memory, subsetting for date range...")
 eda1data_subset <- eda1data_full[(eda1data_full$Date== dateFrom | data_full$Date== dateTo ), ]
 
 # Just plot as hitorgram per assignment
@@ -34,7 +35,7 @@ hist(eda1data_subset$Global_active_power, main="Global Active Power 1/2/2007 to 
 
 # export to a png file by copying
 message ("Copying plot to file")
-dev.copy(png, file="ragplot1.png", height=480, width=480)
+dev.copy(png, file=outPutFile, height=480, width=480)
 # close the device/file
 dev.off()
 message("all done")
